1835227999d3a1ed617dd2f0ad11372b4b57a4bf
Dhaval Jani <dhavaljani1990@gmail.com>
2016-11-23 10:53:57 +0530

    create google_drive_config.php to config the google drive

---
+++ api/v1/google_drive_config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("CLIENT_ID", '799195806593-f2929sbbhh2772c0jfs3a0fbviecfgf4.apps.googleusercontent.com');
+
+define("CLIENT_SECRET", '6Au-DeI2aP58VgyW9OSWhIR8');
+
+define("UNIQUE_FOLDER_ID", '0B0eA1iZuKcaoZzVaT09VTWZGa0E');
+define("REDIRECT_URL", 'https://localhost:8444/api/v1/drive/auth');
+
+?>
